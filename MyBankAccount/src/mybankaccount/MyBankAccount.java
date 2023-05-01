38d36e1106852bafd7ca32e04f96c968c85976a1
Aristeidis Stagkos <115788364+Stagkos2034@users.noreply.github.com>
2023-05-02 00:21:29 +0300

    Add files via upload

---
+++ MyBankAccount/src/mybankaccount/MyBankAccount.java
@@ -0,0 +1,17 @@
+
+package mybankaccount;
+
+import java.io.IOException;
+
+
+public class MyBankAccount {
+
+
+    public static void main(String[] args) throws IOException {
+        Wallet myWalletie = new Wallet(124.89,127.72);//Object
+        Wallet.myWallet(124.89, 127.72);//Calling method
+        
+        
+    }
+    
+}
